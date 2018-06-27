bd6a644f6ff9b11a16be3822a357aad039e98765
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-06-27 12:32:23 +0700

    Init with esint and linear search

---
+++ algor/search/linear.js
@@ -0,0 +1,22 @@
+/**
+ *
+ *
+ * @param {Array<Number>} arr
+ * @param {Number} target
+ * @returns {Number} index
+ */
+function linearSearch(arr = [], target) {
+    if (!arr.length) {
+        return -1
+    }
+
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] === target) {
+            return i
+        }
+    }
+
+    return -1
+}
+
+console.log(linearSearch([1, 2, 3], 3))
