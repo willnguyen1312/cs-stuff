d4a19f53e8129b7d9fb51ae0167b3a89f4658961
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-06-29 13:37:10 +0700

    Add more tests to get 100% coverage

--- src/challenges/hackerrank/birthdayCakeCandles.test.js
+++ src/challenges/hackerrank/birthdayCakeCandles.test.js
@@ -3,9 +3,16 @@ import {
 } from "./birthdayCakeCandles"
 
 describe('BirthdayCakeCandles function', () => {
-    it('should work', () => {
+    it('should pass the first test', () => {
         const arrInput = [3, 2, 1, 3]
         const expectedOutput = 2
+
+        expect(birthdayCakeCandles(arrInput)).toBe(expectedOutput)
+    })
+
+    it('should work', () => {
+        const arrInput = [3, 2, 1, 3, 4, 5]
+        const expectedOutput = 1
 
         expect(birthdayCakeCandles(arrInput)).toBe(expectedOutput)
     })
