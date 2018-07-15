6478291cc972ab104462cec555bb0ac2c0e24795
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-07-15 20:26:57 +0700

    Add selectionSort

---
+++ src/algor/sort/selectionSort.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+function swap(items, firstIndex, secondIndex) {
+    const temp = items[firstIndex]
+    items[firstIndex] = items[secondIndex]
+    items[secondIndex] = temp
+}
+
+function selectionSort(items) {
+
+    const len = items.length
+
+
+    let min
+
+    for (i = 0; i < len; i++) {
+
+        // set minimum to this position
+        min = i
+
+        // check the rest of the array to see if anything is smaller
+        for (j = i + 1; j < len; j++) {
+            if (items[j] < items[min]) {
+                min = j
+            }
+        }
+
+        // if the minimum isn't in the position, swap it
+        if (i != min) {
+            swap(items, i, min)
+        }
+    }
+
+    return items
+}
